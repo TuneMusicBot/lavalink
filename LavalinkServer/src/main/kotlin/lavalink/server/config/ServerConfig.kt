d889a20cef70eb1924661f1955111eff20128733
David Amorim <davidfilipeamorim@gmail.com>
2021-07-08 19:06:56 +0100

    Remove sentryDsn deprecated property

--- LavalinkServer/src/main/kotlin/lavalink/server/config/ServerConfig.kt
+++ LavalinkServer/src/main/kotlin/lavalink/server/config/ServerConfig.kt
@@ -29,8 +29,6 @@ import org.springframework.stereotype.Component
 @ConfigurationProperties(prefix = "lavalink.server")
 @Component
 class ServerConfig {
-  @get:Deprecated("use {@link SentryConfigProperties} instead.")
-  var sentryDsn = ""
   var playerUpdateInterval: Int = 5
   var password: String? = ""
 }
