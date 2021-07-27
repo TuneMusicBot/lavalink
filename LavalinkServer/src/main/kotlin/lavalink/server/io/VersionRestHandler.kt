dc963d7a7bcff1acdd51008f6c0824c985680ece
David Amorim <davidfilipeamorim@gmail.com>
2021-07-27 16:51:46 +0100

    Remove unused import

--- LavalinkServer/src/main/kotlin/lavalink/server/io/VersionRestHandler.kt
+++ LavalinkServer/src/main/kotlin/lavalink/server/io/VersionRestHandler.kt
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
-import java.time.Instant
 import javax.servlet.http.HttpServletRequest
 
 @RestController
